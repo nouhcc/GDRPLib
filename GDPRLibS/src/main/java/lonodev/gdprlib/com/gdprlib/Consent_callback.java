18763cfd277537fb52b8e4f4f894fc757ae78cb2
nouhcc <nouhcc@hotmail.fr>
2018-06-08 15:52:52 +0100

    creat project GDRPLib

---
+++ GDPRLibS/src/main/java/lonodev/gdprlib/com/gdprlib/Consent_callback.java
@@ -0,0 +1,9 @@
+package lonodev.gdprlib.com.gdprlib;
+
+/**
+ * Created by nouhcc on 05/06/2018.
+ */
+
+public interface Consent_callback {
+    void NextActivity();
+}
---
+++ GDPRLibS/src/main/res/Consent_callback.java
@@ -0,0 +1,9 @@
+package android.gdpr_custom_provider_example.com.gdprlib;
+
+/**
+ * Created by nouhcc on 05/06/2018.
+ */
+
+public interface Consent_callback {
+    void NextActivity();
+}
